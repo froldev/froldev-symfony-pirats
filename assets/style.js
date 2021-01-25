8c546e3af174c01b6c41ffd6fe2c066d0f6544f9
Fred OLIVE <frol.developpement@gmail.com>
2021-01-25 21:36:23 +0100

    modify webpack encore with 2 folders : map and style

--- assets/style.js
+++ assets/style.js
@@ -7,7 +7,6 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './css/style.css';
-import './css/map.css';
 
 // start the Stimulus application
 import './bootstrap';
